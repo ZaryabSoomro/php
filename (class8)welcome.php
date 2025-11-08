e56cddf16e637829f869ab1cd51489ad11cff0c6
ZaryabSoomro <zaryabsoomro1@outlook.com>
2025-11-08 16:22:21 +0500

    first commit

---
+++ (class8)welcome.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        session_start();
+    ?>
+    <h1>Class 8 (authentication)</h1>
+    <h2>WELCOME <?php echo $_SESSION['get_user_name'] ?></h2>
+</body>
+</html>
